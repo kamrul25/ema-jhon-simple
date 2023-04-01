fb1bd4cfe4abfdba8cf5ebdd0855f4c6a79160f0
kamrul25 <kamrul.coder01@gmail.com>
2023-04-02 00:31:46 +0600

    first commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data => setProducts(data))
+    }, []);
+
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for(const id in storedCart){
+            // step 2:get product from products state by using id
+            const addedProduct = products.find(product => product.id === id );
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            // console.log('added Product', addedProduct);
+        }
+        setCart(savedCart);
+    },[products]);
+
+    const handleAddToCart = (product) => {
+        // cart.push(product)
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1 
+        // if exist update quantity by 1
+        const exist = cart.find(pd => pd.id === product.id);
+        if(!exist){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exist.quantity = exist.quantity + 1;
+            const remaining = cart.find(pd => pd.id !== product.id);
+            newCart = [...remaining, exist];
+        }
+
+        setCart(newCart);
+        addToDb(product.id)
+    }
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                {
+                    products.map(product => <Product 
+                    key={product.id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}    
+                    ></Product>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
